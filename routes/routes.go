ac698912befa299f60d197adf334dbfe0bf74752
yoBoiRey <167836245+yoBoiRey@users.noreply.github.com>
2025-10-31 14:18:02 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -7,5 +7,9 @@ import (
 )
 
 func SetupRoutes(router *gin.Engine) {
+	// Endpoint CRUD Program BPJS
 	router.GET("/programs", controllers.GetPrograms)
+	router.POST("/programs", controllers.CreateProgram)
+	router.PUT("/programs/:id", controllers.UpdateProgram)
+	router.DELETE("/programs/:id", controllers.DeleteProgram)
 }
